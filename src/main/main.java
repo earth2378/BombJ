8284cefcd6c4899c079b723edb890a6f78835969
earth2378 <earth2378@hotmail.com>
2016-12-03 13:22:26 +0700

    main

--- src/main/main.java
+++ src/main/main.java
@@ -1,6 +1,7 @@
 package main;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import ui.GameScreen;
@@ -9,8 +10,9 @@ public class Main extends Application{
 
 	public static Main instance;
 	private Stage primaryStage;
-	private GameScreen startScreen;
-	private Scene startScene;
+	private GameScreen startScreen,gameOpen;
+	private Scene startScene,openScene;
+	public boolean openGame = false;
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -23,8 +25,19 @@ public class Main extends Application{
 		startScene = new Scene(startScreen);
 		primaryStage.setScene(startScene);
 		primaryStage.show();
+		
+		
 	}
 	public static void main(String[] args) {
 		Application.launch(args);
+		
+	}
+	public void gameStart(){
+		if(openGame){
+			gameOpen = new GameScreen();
+			openScene = new Scene(gameOpen);
+			this.primaryStage.setScene(openScene);
+			
+		}
 	}
 }
