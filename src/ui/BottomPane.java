4d9e33acf33b958c1aa4fb5d33bc1fdcc6859113
earth2378 <earth2378@hotmail.com>
2016-12-03 13:22:08 +0700

    stage

--- src/ui/BottomPane.java
+++ src/ui/BottomPane.java
@@ -1,24 +1,19 @@
 package ui;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import main.Main;
 
+public class BottomPane extends GridPane {
+	private Button start, exit, instruction;
 
-public class BottomPane extends GridPane{
-	private Button start,exit,instruction;
-	
-
-	
-	public BottomPane(){
+	public BottomPane() {
 		this.setPrefSize(800, 50);
-		
+
 		this.setAlignment(Pos.CENTER);
 		this.setStyle("-fx-background-image: url('img/bottomBG.png')");
 		start = new Button("Let's Go!!");
@@ -31,23 +26,38 @@ public class BottomPane extends GridPane{
 		this.add(start, 0, 0);
 		this.add(exit, 2, 0);
 		this.add(instruction, 1, 0);
-		
-		
-		
-		
-		//Set on action
+
+		// Set on action
 		instruction.setOnAction(new EventHandler<ActionEvent>() {
-			
+
 			@Override
 			public void handle(ActionEvent event) {
 				// TODO Auto-generated method stub
-				if(instruction.getText() == "How to play?"){
+				if (instruction.getText() == "How to play?") {
 					GameScreen.setBackground(2);
 					instruction.setText("I got it!");
-				}else if(instruction.getText() == "I got it!"){
+				} else if (instruction.getText() == "I got it!") {
 					GameScreen.setBackground(1);
 					instruction.setText("How to play?");
 				}
+			}
+		});
+		exit.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				Platform.exit();
+			}
+		});
+		start.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				Main.instance.openGame = true;
+				
+				Main.instance.gameStart();
 			}
 		});
 	}
