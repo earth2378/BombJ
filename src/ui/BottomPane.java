edc13aa1580f83a965a808aaba42d9c909108d22
earth2378 <earth2378@hotmail.com>
2016-12-03 12:39:22 +0700

    hello

--- src/ui/BottomPane.java
+++ src/ui/BottomPane.java
@@ -1,25 +1,54 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+
 
 public class BottomPane extends GridPane{
 	private Button start,exit,instruction;
 	
+
+	
 	public BottomPane(){
-		this.setPrefSize(800, 100);
+		this.setPrefSize(800, 50);
+		
 		this.setAlignment(Pos.CENTER);
+		this.setStyle("-fx-background-image: url('img/bottomBG.png')");
 		start = new Button("Let's Go!!");
 		exit = new Button("Good Bye T_T");
 		instruction = new Button("How to play?");
 		start.setPrefSize(150, 30);
 		exit.setPrefSize(150, 30);
 		instruction.setPrefSize(150, 30);
-		this.setHgap(50);
+		this.setHgap(100);
 		this.add(start, 0, 0);
-		this.add(exit, 1, 0);
-		this.add(instruction, 2, 0);
+		this.add(exit, 2, 0);
+		this.add(instruction, 1, 0);
 		
+		
+		
+		
+		//Set on action
+		instruction.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				if(instruction.getText() == "How to play?"){
+					GameScreen.setBackground(2);
+					instruction.setText("I got it!");
+				}else if(instruction.getText() == "I got it!"){
+					GameScreen.setBackground(1);
+					instruction.setText("How to play?");
+				}
+			}
+		});
 	}
 }
--- src/ui/GameScreen.java
+++ src/ui/GameScreen.java
@@ -1,14 +1,25 @@
 package ui;
 
-import javafx.scene.control.Button;
+import javafx.geometry.Insets;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 
 public class GameScreen extends BorderPane{
-	
+	private Canvas canvas ;
+	protected static GraphicsContext gc;
 	
 	public GameScreen(){
-		this.setPrefSize(800, 800);
+		this.setPrefSize(780, 630);
+		this.setPadding(new Insets(0));
+		canvas = new Canvas(800,650);
+		gc = canvas.getGraphicsContext2D();
+		this.getChildren().add(canvas);
+		Image bg = new Image(ClassLoader.getSystemClassLoader().getResource("img/bg2.jpg").toString());
+		gc.drawImage(bg, 0, 0);
 		BottomPane bottomPane = new BottomPane();
 		this.setBottom(bottomPane);
 		
@@ -16,4 +27,17 @@ public class GameScreen extends BorderPane{
 		
 		
 	}
+	public static void setBackground(int type){
+		gc.clearRect(0, 0, 800, 650);
+		if(type == 1){
+			Image bg = new Image(ClassLoader.getSystemClassLoader().getResource("img/bg2.jpg").toString());
+			gc.drawImage(bg, 0, 0);
+		}else if (type == 2){
+			gc.setFill(Color.AQUA);
+			gc.fillRect(0, 0, 800, 650);
+		}
+		
+	}
+	
+	
 }
