5c9c87b3888c853e06c40288404d8191d3fc186e
earth2378 <earth2378@hotmail.com>
2016-12-08 11:06:01 +0700

    paintsb

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -3,15 +3,19 @@ package main;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import lib.IRenderableObject;
 import lib.RenderableHolder;
+import model.Player;
+import model.Player1;
+import model.Player2;
 
 public class GameWindow extends Canvas {
 	
 	public static int screen_width, screen_height;
 	
 	public GameWindow(int width,int height) {
-		screen_height = height;
+		screen_height = height+100;
 		screen_width = width;
 	}
 	
@@ -25,6 +29,28 @@ public class GameWindow extends Canvas {
 		
 		for(IRenderableObject i : RenderableHolder.getInstance().getEntities()){
 			i.render(gc);
+		}
+	}
+	public void paintStatusBar(){
+		GraphicsContext gc = this.getGraphicsContext2D();
+		gc.setFill(Color.DARKGRAY);
+		gc.fillRect(0, 660, 660, 100);
+		for(int i=0;i<RenderableHolder.getInstance().getEntities().size();i++){
+			if(RenderableHolder.getInstance().getEntities().get(i) instanceof Player1){
+				int life = ((Player1)RenderableHolder.getInstance().getEntities().get(i)).getLife();
+				int location = 5;
+				for(int j = 0; j < life ; j+=2){
+					gc.drawImage(RenderableHolder.hearthead, location, 715);
+					location += 40;
+				}
+			}else if(RenderableHolder.getInstance().getEntities().get(i) instanceof Player2){
+				int life = ((Player2)RenderableHolder.getInstance().getEntities().get(i)).getLife();
+				int location = 615;
+				for(int j = 0; j < life ; j+=2){
+					gc.drawImage(RenderableHolder.hearthead, location, 715);
+					location -= 40;
+				}
+			}
 		}
 	}
 }
